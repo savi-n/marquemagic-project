543d031c1ca0a9017d2eea5e3b38f68fa6028ace
Sharook Ibnu Abdul Kadar <sharook.a@namastecredit.com>
2021-06-28 17:58:41 +0530

    fixes

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,24 +2,26 @@ import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import Loading from "./Loading";
+import { APP_DOMAIN } from "../_config/app.config";
 
 const Products = lazy(() => import("../pages/products/Products"));
 const Product = lazy(() => import("../pages/product/Product"));
 
 export default function Content() {
+  console.log(APP_DOMAIN);
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={`${APP_DOMAIN}`}>
         <Suspense fallback={<Loading />}>
           <Switch>
-            <Route exact path="/applyloan" component={Products} />
+            <Route exact path={`/applyloan`} component={Products} />
             <Route
-              path="/product/:product"
+              path={`/product/:product`}
               component={({ match }) => (
                 <Product product={match.params.product} url={match.url} />
               )}
             />
-            <Route render={() => <Redirect to="/applyloan" />} />
+            <Route render={() => <Redirect to={`/applyloan`} />} />
           </Switch>
         </Suspense>
       </BrowserRouter>
