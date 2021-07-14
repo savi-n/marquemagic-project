aeeeffee4ca9ae465da237ee11db672599b58b8b
Sharook Ibnu Abdul Kadar <sharook.a@namastecredit.com>
2021-07-14 15:08:00 +0530

    checkbox agrement disable button

--- src/pages/product/documentUpload/CubDocumentUpload.js
+++ src/pages/product/documentUpload/CubDocumentUpload.js
@@ -231,7 +231,7 @@ export default function DocumentUpload({
   };
 
   const buttonDisabledStatus = () => {
-    return caseCreationProgress;
+    return caseCreationProgress || !declareCheck;
     // return (
     //   caseCreationProgress ||
     //   !(!!userType || bankCUBStatementFetchDone) ||
