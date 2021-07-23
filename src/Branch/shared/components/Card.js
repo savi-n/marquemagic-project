ffacb20e0d01499ce5981150c20dc3cbd1766ab7
aditi.konde <aditi.konde@namastecredit.com>
2021-07-23 18:43:26 +0530

    task 285 - loan_ref_id added

--- src/Branch/shared/components/Card.js
+++ src/Branch/shared/components/Card.js
@@ -38,7 +38,7 @@ export default function Card(props) {
 		<main
 			style={{
 				boxShadow: '0 0 19px 0px #98AFC7',
-				height: `${getter() ? 'auto' : small ? 'auto' : medium ? '16rem' : '22rem'}`,
+				height: `${getter() ? 'auto' : small ? 'auto' : medium ? '16rem' : '23rem'}`,
 				maxHeight: `${getter() ? '80rem' : 'auto'}`,
 				width: `${!full ? 'calc(100%/3)' : '100%'}`,
 				maxWidth: `${getter() ? '100%' : '100%'}`
