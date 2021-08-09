8723ac1b25cb2ba8bda10b5374f55ab417b29719
pradeep.kumar <pradeep.kumar@namastecredit.com>
2021-08-09 17:52:05 +0530

    bug fixes

--- src/components/ROCBusinessDetailsModal.js
+++ src/components/ROCBusinessDetailsModal.js
@@ -50,59 +50,59 @@ export default function ROCBusinessDetailsModal({ onClose }) {
 			<Div>
 				<Row>
 					<Colm1>Business Name</Colm1>
-					<Colm2>{companyDetail.BusinessName}</Colm2>
+					<Colm2>{companyDetail?.BusinessName}</Colm2>
 				</Row>
 
 				<Row>
 					<Colm1>Email</Colm1>
-					<Colm2>{companyDetail.Email}</Colm2>
+					<Colm2>{companyDetail?.Email}</Colm2>
 				</Row>
 				<Row>
 					<Colm1>Address</Colm1>
-					{!isDemo && <Colm2>{companyDetail.Address}</Colm2>}
+					{!isDemo && <Colm2>{companyDetail?.Address}</Colm2>}
 					{isDemo && (
 						<section className='flex flex-col items-center'>
-							<span>{companyDetail.Address.bno}</span>
-							<span>{companyDetail.Address.st}</span>
-							<span>{companyDetail.Address.loc}</span>
-							<span>{companyDetail.Address.dst}</span>
-							<span>{companyDetail.Address.pncd}</span>
-							<span>{companyDetail.Address.stcd}</span>
+							<span>{companyDetail?.Address.bno}</span>
+							<span>{companyDetail?.Address.st}</span>
+							<span>{companyDetail?.Address.loc}</span>
+							<span>{companyDetail?.Address.dst}</span>
+							<span>{companyDetail?.Address.pncd}</span>
+							<span>{companyDetail?.Address.stcd}</span>
 						</section>
 					)}
 				</Row>
 
 				<Row>
 					<Colm1>Pancard Number</Colm1>
-					<Colm2>{companyDetail.PancardNumber}</Colm2>
+					<Colm2>{companyDetail?.PancardNumber}</Colm2>
 				</Row>
 
 				<Row>
 					<Colm1>CIN</Colm1>
-					<Colm2>{companyDetail.CIN}</Colm2>
+					<Colm2>{companyDetail?.CIN}</Colm2>
 				</Row>
 
 				<Row>
 					<Colm1>Email</Colm1>
-					<Colm2>{companyDetail.Email}</Colm2>
+					<Colm2>{companyDetail?.Email}</Colm2>
 				</Row>
 				<Row>
 					<Colm1>Registration Number</Colm1>
-					<Colm2>{companyDetail.RegistrationNumber}</Colm2>
+					<Colm2>{companyDetail?.RegistrationNumber}</Colm2>
 				</Row>
 				<Row>
 					<Colm1>Company Category</Colm1>
-					<Colm2>{companyDetail.CompanyCategory}</Colm2>
+					<Colm2>{companyDetail?.CompanyCategory}</Colm2>
 				</Row>
 
-				{companyDetail.directorsForShow.length && (
+				{companyDetail?.directorsForShow.length && (
 					<>
 						<Header>Directors/Partners</Header>
 						<Row>
 							<Colm1>Name</Colm1>
 							<Colm2>Din</Colm2>
 						</Row>
-						{companyDetail.directorsForShow.map((dir, index) => (
+						{companyDetail?.directorsForShow.map((dir, index) => (
 							<Row key={index}>
 								<Colm1>{dir.Name}</Colm1>
 								<Colm2>{dir.Din}</Colm2>
