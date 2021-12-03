5d1cc18d31ec6c4cfbbe364b1a86e4f7f6a728db
brihaspati <brihaspati.pandey@namastecredit.com>
2021-12-03 05:55:11 +0000

    Revert "Merge branch 'redux-onboarding' into 'master'"
    This reverts merge request !84

--- src/pages/product/Router.js
+++ src/pages/product/Router.js
@@ -5,46 +5,24 @@ import userType from '../../_hoc/userType';
 import Loading from '../../components/Loading';
 
 const ProductDetails = lazy(() => import('./productDetails/ProductDetails'));
-const IdentityVerification = lazy(() =>
-	import('./identityVerification/IdentityVerification')
-);
+const IdentityVerification = lazy(() => import('./identityVerification/IdentityVerification'));
 
-const BusinessVerification = lazy(() =>
-	import('./bussinessVerification/BussinessVerification')
-);
+const BusinessVerification = lazy(() => import('./bussinessVerification/BussinessVerification'));
 
-const PanVerification = lazy(() =>
-	import('./panverification/pan-verifications')
-);
+const PanVerification = lazy(() => import('./panverification/pan-verifications'));
 
-const DocumentUpload = lazy(() =>
-	import('./documentUpload/LoanDocumentsUpload')
-);
-const CubDocumentUpload = lazy(() =>
-	import('./documentUpload/CubDocumentUpload')
-);
+const DocumentUpload = lazy(() => import('./documentUpload/LoanDocumentsUpload'));
+const CubDocumentUpload = lazy(() => import('./documentUpload/CubDocumentUpload'));
 const PersonalDetails = lazy(() => import('./personalDetails/PersonalDetails'));
 const AddressDetails = lazy(() => import('./addressDetails/AddressDetails'));
-const LoanAddressDetails = lazy(() =>
-	import('./addressDetails/LoanAddressDetails')
-);
+const LoanAddressDetails = lazy(() => import('./addressDetails/LoanAddressDetails'));
 
-const ApplicationSubmitted = lazy(() =>
-	import('./applicationSubmitted/ApplicationSubmitted')
-);
-const TwoWheelerLoanDetails = lazy(() =>
-	import('./loanDetails/TwoWheelerLoanDetails')
-);
-const FourWheelerLoanDetails = lazy(() =>
-	import('./loanDetails/FourWheelerLoanDetails')
-);
+const ApplicationSubmitted = lazy(() => import('./applicationSubmitted/ApplicationSubmitted'));
+const TwoWheelerLoanDetails = lazy(() => import('./loanDetails/TwoWheelerLoanDetails'));
+const FourWheelerLoanDetails = lazy(() => import('./loanDetails/FourWheelerLoanDetails'));
 const HomeLoanDetails = lazy(() => import('./loanDetails/HomeLoanDetails'));
-const CoApplicantDetails = lazy(() =>
-	import('./coappilcant/CoapplicantDetails')
-);
-const CoApplicantIncomeDetails = lazy(() =>
-	import('./coappilcant/CoapplicantIncomeDetails')
-);
+const CoApplicantDetails = lazy(() => import('./coappilcant/CoapplicantDetails'));
+const CoApplicantIncomeDetails = lazy(() => import('./coappilcant/CoapplicantIncomeDetails'));
 const EmiDetails = lazy(() => import('./emiDetails/EMIDetails'));
 
 const FormDefaultPage = lazy(() => import('./formPage/FormController'));
@@ -61,29 +39,29 @@ const availableRoutes = {
 	'loan-address-details': { protected: true, Component: LoanAddressDetails },
 	'two-wheeler-loan-details': {
 		protected: true,
-		Component: TwoWheelerLoanDetails,
+		Component: TwoWheelerLoanDetails
 	},
 	'four-wheeler-loan-details': {
 		protected: true,
-		Component: FourWheelerLoanDetails,
+		Component: FourWheelerLoanDetails
 	},
 	'loan-details': {
 		protected: true,
-		Component: FourWheelerLoanDetails,
+		Component: FourWheelerLoanDetails
 	},
 	'home-loan-details': { protected: true, Component: HomeLoanDetails },
 	'lap-loan-details': { protected: true, Component: LapLoanDetails },
 	'co-applicant-details': {
 		protected: true,
-		Component: userType('Co-applicant', CoApplicantDetails),
+		Component: userType('Co-applicant', CoApplicantDetails)
 	},
 	'co-applicant-income-details': {
 		protected: true,
-		Component: userType('Co-applicant', CoApplicantIncomeDetails),
+		Component: userType('Co-applicant', CoApplicantIncomeDetails)
 	},
 	'co-applicant-document-upload': {
 		protected: true,
-		Component: userType('Co-applicant', CubDocumentUpload),
+		Component: userType('Co-applicant', CubDocumentUpload)
 	},
 	'emi-details': { protected: true, Component: EmiDetails },
 	'document-upload': { protected: true, Component: DocumentUpload },
@@ -91,16 +69,16 @@ const availableRoutes = {
 	'application-submitted': { protected: true, Component: ApplicationSubmitted },
 	'guarantor-details': {
 		protected: true,
-		Component: userType('Guarantor', CoApplicantDetails),
+		Component: userType('Guarantor', CoApplicantDetails)
 	},
 	'guarantor-income-details': {
 		protected: true,
-		Component: userType('Guarantor', CoApplicantIncomeDetails),
+		Component: userType('Guarantor', CoApplicantIncomeDetails)
 	},
 	'guarantor-document-upload': {
 		protected: true,
-		Component: userType('Guarantor', CubDocumentUpload),
-	},
+		Component: userType('Guarantor', CubDocumentUpload)
+	}
 };
 
 // const fieldConfig = {
@@ -109,19 +87,13 @@ const availableRoutes = {
 //   "Home Loan": require("../../shared/constants/homeFields.json"),
 // };
 
-export default function Router({
-	currentFlow,
-	productDetails = {},
-	map,
-	onFlowChange,
-	productId,
-}) {
+export default function Router({ currentFlow, productDetails = {}, map, onFlowChange, productId }) {
 	if (!currentFlow) {
 		return <Loading />;
 	}
 
 	const component = availableRoutes[currentFlow] || {
-		Component: FormDefaultPage,
+		Component: FormDefaultPage
 	};
 
 	return (
@@ -143,5 +115,5 @@ Router.propTypes = {
 	productDetails: object,
 	onFlowChange: func,
 	map: oneOfType([string, object]),
-	productId: object.isRequired,
+	productId: object.isRequired
 };
