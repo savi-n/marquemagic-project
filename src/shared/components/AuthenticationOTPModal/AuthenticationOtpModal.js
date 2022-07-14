90391224032021364dd0062384ed35f711a53fbc
Deepika Upadhyay <deepika.upadhyay@namastecredit.com>
2022-07-14 15:43:33 +0530

    dos-1428

--- src/shared/components/AuthenticationOTPModal/AuthenticationOtpModal.js
+++ src/shared/components/AuthenticationOTPModal/AuthenticationOtpModal.js
@@ -197,6 +197,11 @@ const AuthenticationOTPModal = props => {
 					message: 'OTP generated again',
 					type: 'success',
 				});
+			} else {
+				addToast({
+					message: authenticationResendOtpRes.data.message,
+					type: 'error',
+				});
 			}
 		} catch (error) {
 			console.log(error);
