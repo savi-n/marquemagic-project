62f364a5a9f54bd2de61c1a1fac9b6c8368a442c
Varun Miyani <varunbhai.miyani@namastecredit.com>
2022-11-28 12:55:27 +0530

    address details comment removed

--- src/components/Sections/AddressDetails/AddressDetails.js
+++ src/components/Sections/AddressDetails/AddressDetails.js
@@ -1,7 +1,7 @@
 //aid:1 = present address
 //aid:2 = permanent address
 
-import React, { useState, Fragment, useEffect } from 'react';
+import React, { useState, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import _ from 'lodash';
@@ -15,9 +15,7 @@ import {
 	// setIsSameAsAboveAddressChecked,
 	updateApplicantSection,
 	updateCoApplicantSection,
-	setPresentAddressProofExtractionRes,
 	setGenerateAadhaarOtpResponse,
-	removeCacheDocument,
 	addCacheDocuments,
 } from 'store/applicantCoApplicantsSlice';
 import { setSelectedSectionId } from 'store/appSlice';
@@ -81,7 +79,6 @@ const AddressDetails = props => {
 		onChangeFormStateField,
 	} = useForm();
 	const [loading, setLoading] = useState(false);
-	const [fetchingAddress, setFetchingAddress] = useState(false);
 	const [verifyingWithOtp, setVerifyingWithOtp] = useState(false);
 	const [
 		permanentCacheDocumentsTemp,
@@ -111,206 +108,6 @@ const AddressDetails = props => {
 		application,
 	});
 	const isSectionCompleted = completedSections.includes(selectedSectionId);
-	// const addCacheDocumentTemp = file => {
-	// 	const newCacheDocumentTemp = _.cloneDeep(cacheDocumentsTemp);
-	// 	newCacheDocumentTemp.push(file);
-	// 	setCacheDocumentsTemp(newCacheDocumentTemp);
-	// };
-
-	// const addCacheDocumentsTemp = files => {
-	// 	const newCacheDocumentTemp = _.cloneDeep(cacheDocumentsTemp);
-	// 	files.map(doc => {
-	// 		newCacheDocumentTemp.push({ ...doc });
-	// 		return null;
-	// 	});
-	// 	setCacheDocumentsTemp(newCacheDocumentTemp);
-	// };
-
-	// const removeCacheDocumentTemp = fieldName => {
-	// 	// console.log('removeCacheDocumentTemp-', { fieldName, cacheDocumentsTemp });
-	// 	const newCacheDocumentTemp = _.cloneDeep(cacheDocumentsTemp);
-	// 	if (
-	// 		cacheDocumentsTemp.filter(doc => doc.field.name === fieldName).length > 0
-	// 	) {
-	// 		setCacheDocumentsTemp(
-	// 			newCacheDocumentTemp.filter(doc => doc.field.name !== fieldName)
-	// 		);
-	// 	} else {
-	// 		dispatch(removeCacheDocument({ fieldName }));
-	// 	}
-	// };
-
-	// const handleFileUploadAddressProof = (files, isPermanent) => {
-	// 	const newCacheDocumentTemp = _.cloneDeep(cacheDocumentsTemp);
-	// 	files.map(f => newCacheDocumentTemp.push(f));
-	// 	setCacheDocumentsTemp(newCacheDocumentTemp);
-	// 	// isPermanent
-	// 	// ? setPermanentAddressProofError('')
-	// 	// : setPresentAddressProofError('');
-
-	// 	// let newAddressProofDocs = _.cloneDeep(presentAddressProofDocsRef.current);
-	// 	// files.map(f => newAddressProofDocs.push(_.cloneDeep({ ...f, aid })));
-	// 	// newAddressProofDocs = _.uniqBy(newAddressProofDocs, function(e) {
-	// 	// 	return e.id;
-	// 	// });
-	// 	// // console.log('pan-verification-handleFileUploadAddressProof-', {
-	// 	// //  files,
-	// 	// //  newAddressProofDocs,
-	// 	// //  addressProofDocs,
-	// 	// //  selectedAddressProof,
-	// 	// // });
-	// 	// setPresentAddressProofDocs(newAddressProofDocs);
-	// 	// presentAddressProofDocsRef.current = newAddressProofDocs;
-	// 	// // setDisableSubmit(false);
-	// 	// setPresentAddressProofError('');
-	// };
-
-	// const handleFileRemoveAddressProof = (docId, isPermanent) => {
-	// 	const newCacheDocumentTemp = _.cloneDeep(cacheDocumentsTemp).filter(
-	// 		doc => doc.id !== docId
-	// 	);
-	// 	setCacheDocumentsTemp(newCacheDocumentTemp);
-	// 	// isPermanent
-	// 	// ? setPermanentAddressProofError('')
-	// 	// : setPresentAddressProofError('');
-	// 	// console.log('handleFileRemoveAddressProof docId-', docId);
-	// 	// dispatch(removeLoanDocument(docId));
-	// 	// const newAddressProofDocs = _.cloneDeep(
-	// 	// 	// eslint-disable-next-line
-	// 	// 	fileRef.current.filter(f => f.id != docId)
-	// 	// );
-	// 	// fileRef.current = newAddressProofDocs;
-	// 	// const newAddressProofDocs = _.cloneDeep(
-	// 	// 	presentAddressProofDocsRef.current.filter(f => f.id !== docId)
-	// 	// );
-	// 	// setPresentAddressProofDocs(newAddressProofDocs);
-	// 	// presentAddressProofDocsRef.current = newAddressProofDocs;
-	// };
-
-	// const handleDocumentTypeChangeAddressProof = async (fileId, type) => {
-	// 	const newCacheDocumentTemp = [];
-	// 	cacheDocumentsTemp.map(doc => {
-	// 		const newDoc = _.cloneDeep(doc);
-	// 		if (doc.id === fileId) {
-	// 			newDoc.isTagged = type;
-	// 			newDoc.doc_type_id = type.doc_type_id;
-	// 		}
-	// 		newCacheDocumentTemp.push(newDoc);
-	// 		return null;
-	// 	});
-	// 	setCacheDocumentsTemp(newCacheDocumentTemp);
-	// 	// dispatch(updateSelectedDocumentTypeId({ fileId, docType: type }));
-	// 	// const newAddressProofDocs = [];
-	// 	// presentAddressProofDocsRef?.current?.map(f => {
-	// 	// 	const newFile = _.cloneDeep(f);
-	// 	// 	if (f.id === fileId) {
-	// 	// 		newFile.isTagged = type;
-	// 	// 		newFile.doc_type_id = type.id;
-	// 	// 	}
-	// 	// 	newAddressProofDocs.push(newFile);
-	// 	// 	return null;
-	// 	// });
-
-	// 	// console.log('handleDocumentTypeChangeAddressProof-', {
-	// 	// 	addressProofDocs,
-	// 	// 	newAddressProofDocs,
-	// 	// });
-	// 	// // fileRef.current = newAddressProofDocs;
-	// 	// setPresentAddressProofDocs(newAddressProofDocs);
-	// 	// presentAddressProofDocsRef.current = newAddressProofDocs;
-	// };
-
-	// const prepopulateAadhaarAndAddressState = extractionData => {
-	// 	// console.log('prepopulateAadhaarAndAddressState-', extractionData);
-
-	// 	const aadharNum = extractionData?.Aadhar_number?.replaceAll(
-	// 		/\s/g,
-	// 		''
-	// 	).split('');
-	// 	const aadhaarUnMasked = aadharNum?.join('') || '';
-	// 	// const aadhaarMasked = aadharNum
-	// 	// 	? 'XXXXXXXX' + aadharNum?.splice(8, 4).join('')
-	// 	// 	: '';
-	// 	onChangeFormStateField({
-	// 		name: `aadhaar`,
-	// 		value: aadhaarUnMasked,
-	// 	});
-	// 	// const fullName =
-	// 	// 	extractionData?.name?.split(' ') || extractionData?.Name?.split(' ');
-	// 	// const firstName = fullName[0].join(' ');
-	// 	// const lastName = fullName[fullName.length - 1];
-	// 	// const dob = extractionData?.DOB || extractionData?.dob;
-	// 	// const dlNo = extractionData?.dl_no;
-	// 	const fullAddress = extractionData?.address || extractionData?.Address;
-	// 	onChangeFormStateField({
-	// 		name: `${CONST.PREFIX_PRESENT}address1`,
-	// 		value: fullAddress,
-	// 	});
-	// 	const pinCode = extractionData?.pincode;
-	// 	if (fullAddress) {
-	// 		let locationArr = fullAddress && fullAddress?.split(' ');
-	// 		// eslint-disable-next-line
-	// 		let y = locationArr?.map(e => !Number(isNaN(e)) && e);
-	// 		// return Number(e) !== NaN && e;
-	// 		let pin;
-	// 		y.map(e => {
-	// 			if (e?.length === 6) pin = e;
-	// 			return null;
-	// 		});
-
-	// 		const extractedPinCode = pinCode || pin;
-	// 		onChangeFormStateField({
-	// 			name: `${CONST.PREFIX_PRESENT}pin_code`,
-	// 			value: extractedPinCode,
-	// 		});
-	// 	}
-	// };
-
-	// const verifyKycAddressProof = async (
-	// 	selectedAddressProof,
-	// 	extractionData
-	// ) => {
-	// 	try {
-	// 		// console.log('verifyKycAddressProof-', {
-	// 		// 	selectedAddressProof,
-	// 		// 	isVerifyKycData,
-	// 		// 	extractionData,
-	// 		// });
-	// 		if (
-	// 			!selectedProduct?.kyc_verification ||
-	// 			selectedAddressProof === CONST_SECTIONS.EXTRACTION_KEY_AADHAAR
-	// 		)
-	// 			return {};
-	// 		const reqBody = {
-	// 			doc_ref_id: extractionData?.doc_ref_id,
-	// 			doc_type: selectedAddressProof,
-	// 		};
-	// 		if (selectedAddressProof === CONST_SECTIONS.EXTRACTION_KEY_DL) {
-	// 			reqBody.number = extractionData?.dl_no || '';
-	// 			reqBody.dob = extractionData?.dob || extractionData?.DOB || '';
-	// 		}
-	// 		if (selectedAddressProof === CONST_SECTIONS.EXTRACTION_KEY_VOTERID) {
-	// 			reqBody.number = extractionData?.vid || '';
-	// 			reqBody.state = extractionData?.state || '';
-	// 			reqBody.name = extractionData?.Name || extractionData?.name || '';
-	// 		}
-	// 		if (selectedAddressProof === CONST_SECTIONS.EXTRACTION_KEY_PASSPORT) {
-	// 			// TODO: verify by testing passport extraction data
-	// 			reqBody.number = extractionData?.passport_no || '';
-	// 			reqBody.dob = extractionData?.dob || extractionData?.DOB || '';
-	// 			reqBody.name = extractionData?.Name || extractionData?.name || '';
-	// 		}
-	// 		const verifiedRes = await verifyKycDataUiUx(reqBody, clientToken);
-	// 		return verifiedRes;
-	// 	} catch (error) {
-	// 		console.error('error-verifyKycDataUiUx-', error);
-	// 		addToast({
-	// 			message: error.message || 'Something Went Wrong. Try Again!',
-	// 			type: 'error',
-	// 		});
-	// 		return {};
-	// 	}
-	// };
 
 	const onClickVerifyWithOtp = async () => {
 		try {
@@ -359,9 +156,8 @@ const AddressDetails = props => {
 					// console.log(formState.values, '555777');
 					if (verifyOtpResponse.data.address) {
 						// to be continued
-						let address = verifyOtpResponse?.data?.address;
+						// verifyOtpResponse?.data?.address;
 						// population of the data from the aadhaar otp verification is pending here
-
 						// console.log(formState, '111222');
 					}
 				}
@@ -383,228 +179,6 @@ const AddressDetails = props => {
 			setVerifyingWithOtp(false);
 		}
 	};
-
-	// const onClickFetchAddress = async (selectedAddressProofId, prefix) => {
-	// 	try {
-	// 		setLoading(true);
-	// 		setFetchingAddress(true);
-	// 		const REQ_TYPE = selectedAddressProofId.replaceAll(prefix, '');
-	// 		setPresentAddressProofError('');
-	// 		// TODO Filter selected address proof docs before extracting
-	// 		const selectedAddressProofFiles = cacheDocumentsTemp?.filter(
-	// 			f => f?.sectionType === selectedAddressProofId
-	// 		);
-	// 		if (selectedAddressProofFiles.length > 2) {
-	// 			addToast({
-	// 				message: 'Max 2 doucment is allowed',
-	// 				type: 'error',
-	// 			});
-	// 			return;
-	// 		}
-	// 		// console.log(
-	// 		// 	'onClickFetchAddress-selectedAddressProofFiles-',
-	// 		// 	selectedAddressProofFiles
-	// 		// );
-
-	// 		// Front + Back Extract
-	// 		if (selectedAddressProofFiles.length > 1) {
-	// 			const frontFormData = new FormData();
-	// 			frontFormData.append('product_id', selectedProduct.id);
-	// 			frontFormData.append('req_type', REQ_TYPE);
-	// 			frontFormData.append('process_type', 'extraction');
-	// 			frontFormData.append('document', selectedAddressProofFiles[0].file);
-
-	// 			const frontExtractionRes = await getKYCData(frontFormData, clientToken);
-	// 			const frontExtractionStatus = frontExtractionRes?.data?.status || '';
-	// 			const frontExtractionMsg = frontExtractionRes?.data?.message || '';
-	// 			const frontForensicRes = frontExtractionRes?.data?.forensicData || {};
-	// 			const frontForensicFlag = frontForensicRes?.flag?.toLowerCase() || '';
-	// 			const frontForensicFlagMsg = frontForensicRes?.flag_message || '';
-
-	// 			if (frontExtractionStatus === 'nok') {
-	// 				setPresentAddressProofError(
-	// 					`${CONST_SECTIONS.EXTRACTION_FLAG_ERROR}${frontExtractionMsg}`
-	// 				);
-	// 				return; // STOP FURTHER EXECUTION
-	// 			}
-	// 			if (frontForensicFlag === 'error') {
-	// 				setPresentAddressProofError(
-	// 					`${CONST_SECTIONS.EXTRACTION_FLAG_ERROR}${frontForensicFlagMsg}`
-	// 				);
-	// 				return; // STOP FURTHER EXECUTION
-	// 			}
-	// 			if (frontForensicFlag === 'warning') {
-	// 				setPresentAddressProofError(
-	// 					`${CONST_SECTIONS.EXTRACTION_FLAG_WARNING}${frontForensicFlagMsg}`
-	// 				);
-	// 				// CONTINUE EXECUTION
-	// 			}
-
-	// 			// const frontFile = {
-	// 			// 	...(frontExtractionRes?.data?.extractionData || {}),
-	// 			// 	document_key: frontExtractionRes?.data?.s3?.fd,
-	// 			// 	id: selectedAddressProofFiles[0].id,
-	// 			// 	mainType: 'KYC',
-	// 			// 	size: frontExtractionRes?.data?.s3?.size,
-	// 			// 	type: 'other',
-	// 			// 	req_type: selectedPresentAddressProofId, // requires for mapping with JSON
-	// 			// 	requestId: frontExtractionRes?.data?.request_id,
-	// 			// 	upload_doc_name: frontExtractionRes?.data?.s3?.filename,
-	// 			// 	isDocRemoveAllowed: false,
-	// 			// 	category: CONST_SECTIONS.DOC_CATEGORY_KYC,
-	// 			// 	doc_type_id: `app_${CONST_SECTIONS.DOC_CATEGORY_KYC}`,
-	// 			// };
-
-	// 			// TODO: Set doc to Redux
-	// 			// setLoanDocuments([frontFile]);
-	// 			// this ends here
-
-	// 			const backFormData = new FormData();
-	// 			backFormData.append('product_id', selectedProduct.id);
-	// 			backFormData.append('req_type', REQ_TYPE);
-	// 			backFormData.append(
-	// 				'ref_id',
-	// 				frontExtractionRes?.data?.extractionData?.id
-	// 			);
-	// 			backFormData.append('doc_ref_id', frontExtractionRes?.data?.doc_ref_id);
-	// 			backFormData.append('process_type', 'extraction');
-	// 			backFormData.append('document', selectedAddressProofFiles[1].file);
-
-	// 			const backExtractionRes = await getKYCDataId(backFormData, clientToken);
-	// 			const backExtractionStatus = backExtractionRes?.data?.status || '';
-	// 			const backExtractionMsg = backExtractionRes?.data?.message || '';
-	// 			const backForensicRes = backExtractionRes?.data?.forensicData || {};
-	// 			const backForensicFlag = backForensicRes?.flag?.toLowerCase() || '';
-	// 			const backForensicFlagMsg = backForensicRes?.flag_message || '';
-
-	// 			if (backExtractionStatus === 'nok') {
-	// 				setPresentAddressProofError(
-	// 					`${CONST_SECTIONS.EXTRACTION_FLAG_ERROR}${backExtractionMsg}`
-	// 				);
-	// 				return; // STOP FURTHER EXECUTION
-	// 			}
-	// 			if (backForensicFlag === 'error') {
-	// 				setPresentAddressProofError(
-	// 					`${CONST_SECTIONS.EXTRACTION_FLAG_ERROR}${backForensicFlagMsg}`
-	// 				);
-	// 				return; // STOP FURTHER EXECUTION
-	// 			}
-	// 			if (backForensicFlag === 'warning') {
-	// 				setPresentAddressProofError(
-	// 					`${CONST_SECTIONS.EXTRACTION_FLAG_WARNING}${backForensicFlagMsg}`
-	// 				);
-	// 				// CONTINUE EXECUTION
-	// 			}
-
-	// 			// const backFile = {
-	// 			// 	...(backExtractionRes?.data?.extractionData || {}),
-	// 			// 	document_key: backExtractionRes?.data.s3.fd,
-	// 			// 	id: selectedAddressProofFiles[1].id,
-	// 			// 	mainType: 'KYC',
-	// 			// 	size: backExtractionRes?.data.s3.size,
-	// 			// 	type: 'other',
-	// 			// 	req_type: selectedPresentAddressProofId,
-	// 			// 	requestId: backExtractionRes?.data.request_id,
-	// 			// 	upload_doc_name: backExtractionRes?.data.s3.filename,
-	// 			// 	isDocRemoveAllowed: false,
-	// 			// 	category: CONST_SECTIONS.DOC_CATEGORY_KYC,
-	// 			// 	doc_type_id: `app_${CONST_SECTIONS.DOC_CATEGORY_KYC}`,
-	// 			// };
-	// 			// TODO: Set doc to Redux
-	// 			// setLoanDocuments([backFile]);
-	// 			// this ends here
-
-	// 			dispatch(
-	// 				setPresentAddressProofExtractionRes(backExtractionRes?.data || {})
-	// 			);
-	// 			const newAddressProofExtractionData = {
-	// 				...backExtractionRes?.data?.extractionData,
-	// 				doc_ref_id: frontExtractionRes?.data?.doc_ref_id,
-	// 				requestId: backExtractionRes?.data.request_id,
-	// 			};
-	// 			// console.log(newAddressProofExtractionData, '2222');
-	// 			prepopulateAadhaarAndAddressState(newAddressProofExtractionData);
-	// 			await verifyKycAddressProof(REQ_TYPE, newAddressProofExtractionData);
-	// 			return;
-	// 		}
-
-	// 		// Front Only Extract
-	// 		const frontOnlyFormData = new FormData();
-	// 		frontOnlyFormData.append('product_id', selectedProduct.id);
-	// 		frontOnlyFormData.append('req_type', REQ_TYPE);
-	// 		frontOnlyFormData.append('process_type', 'extraction');
-	// 		frontOnlyFormData.append('document', selectedAddressProofFiles[0].file);
-
-	// 		const frontOnlyExtractionRes = await getKYCData(
-	// 			frontOnlyFormData,
-	// 			clientToken
-	// 		);
-	// 		const frontOnlyExtractionStatus =
-	// 			frontOnlyExtractionRes?.data?.status || '';
-	// 		const frontOnlyExtractionMsg =
-	// 			frontOnlyExtractionRes?.data?.message || '';
-	// 		const frontOnlyForensicRes =
-	// 			frontOnlyExtractionRes?.data?.forensicData || {};
-	// 		const frontOnlyForensicFlag =
-	// 			frontOnlyForensicRes?.flag?.toLowerCase() || '';
-	// 		const frontOnlyForensicFlagMsg = frontOnlyForensicRes?.flag_message || '';
-
-	// 		if (frontOnlyExtractionStatus === 'nok') {
-	// 			setPresentAddressProofError(
-	// 				`${CONST_SECTIONS.EXTRACTION_FLAG_ERROR}${frontOnlyExtractionMsg}`
-	// 			);
-	// 			return; // STOP FURTHER EXECUTION
-	// 		}
-	// 		if (frontOnlyForensicFlag === 'error') {
-	// 			setPresentAddressProofError(
-	// 				`${CONST_SECTIONS.EXTRACTION_FLAG_ERROR}${frontOnlyForensicFlagMsg}`
-	// 			);
-	// 			return; // STOP FURTHER EXECUTION
-	// 		}
-	// 		if (frontOnlyForensicFlag === 'warning') {
-	// 			setPresentAddressProofError(
-	// 				`${CONST_SECTIONS.EXTRACTION_FLAG_WARNING}${frontOnlyForensicFlagMsg}`
-	// 			);
-	// 			// CONTINUE EXECUTION
-	// 		}
-
-	// 		const frontOnlyFile = {
-	// 			// field,
-	// 			extractionRes: frontOnlyExtractionRes?.data || {},
-	// 			document_key: frontOnlyExtractionRes?.data?.s3?.fd,
-	// 			id: selectedAddressProofFiles[0].id,
-	// 			mainType: 'KYC',
-	// 			size: frontOnlyExtractionRes?.data?.s3?.size,
-	// 			type: 'other',
-	// 			req_type: REQ_TYPE,
-	// 			requestId: frontOnlyExtractionRes?.data?.request_id,
-	// 			upload_doc_name: frontOnlyExtractionRes?.data?.s3?.filename,
-	// 			isDocRemoveAllowed: false,
-	// 			category: CONST_SECTIONS.DOC_CATEGORY_KYC,
-	// 			doc_type_id: `app_${CONST_SECTIONS.DOC_CATEGORY_KYC}`,
-	// 		};
-	// 		addCacheDocumentTemp(frontOnlyFile);
-	// 		// TODO: Set doc to redux
-	// 		// setLoanDocuments([frontOnlyFile]);
-	// 		// this ends here
-
-	// 		dispatch(
-	// 			setPresentAddressProofExtractionRes(frontOnlyExtractionRes?.data || {})
-	// 		);
-	// 		const newAddressProofExtractionData = {
-	// 			...(frontOnlyExtractionRes?.data?.extractionData || {}),
-	// 			doc_ref_id: frontOnlyExtractionRes?.data?.doc_ref_id,
-	// 			requestId: frontOnlyExtractionRes?.data?.request_id,
-	// 		};
-	// 		prepopulateAadhaarAndAddressState(newAddressProofExtractionData);
-	// 		await verifyKycAddressProof(REQ_TYPE, newAddressProofExtractionData);
-	// 	} catch (error) {
-	// 		console.error('error-pan-verification-onClickFetchAddress-', error);
-	// 	} finally {
-	// 		setLoading(false);
-	// 		setFetchingAddress(false);
-	// 	}
-	// };
 
 	const onProceed = async () => {
 		try {
@@ -1004,7 +578,7 @@ const AddressDetails = props => {
 												addressProofUploadSection={sub_section}
 												selectedApplicant={selectedApplicant}
 												// onClickFetchAddress={onClickFetchAddress}
-												fetchingAddress={fetchingAddress}
+												// fetchingAddress={fetchingAddress}
 												onChangeFormStateField={onChangeFormStateField}
 												prefilledValues={prefilledValues}
 												addressProofError={
