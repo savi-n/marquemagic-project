c58c3646e9e8f42cdc5e7f14f1cc1bdc36ae78f5
bikash.mishra <bikash.mishra@namastecredit.com>
2023-02-21 12:30:06 +0530

    DOS-3326 stoping scroll behaviour in number input

--- src/components/inputs/InputField.js
+++ src/components/inputs/InputField.js
@@ -1,5 +1,6 @@
 /* Input field for Input type */
 
+import { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Input = styled.input`
@@ -110,10 +111,25 @@ const FieldPostfixIcon = styled.span`
 `;
 
 export default function InputField(props) {
+	const quantityInputRef = useRef(null);
+
+	useEffect(() => {
+		const ignoreScroll = e => {
+			e.preventDefault();
+		};
+		quantityInputRef.current &&
+			quantityInputRef.current.addEventListener('wheel', ignoreScroll);
+	}, [quantityInputRef]);
+
 	const isLargeTextLable = props.name.length > 15;
 	return (
 		<Div>
-			<Input id={props.name} type={props.type} {...props} />
+			<Input
+				id={props.name}
+				type={props.type}
+				{...props}
+				ref={quantityInputRef}
+			/>
 			<Label
 				isLargeTextLable={isLargeTextLable}
 				value={props.value}
