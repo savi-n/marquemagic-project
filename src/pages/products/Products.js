bcc847b9f8114fc3c519b0bda9faa02eb31d0e6b
Akshat Sharma~ <akshat.sharma@namastecredit.com~>
2023-04-21 17:06:29 +0530

    DOS-3378

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -19,6 +19,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import Modal from 'components/Modal';
 import Button from 'components/Button';
+import imgClose from 'assets/icons/close_icon_grey-06.svg';
 import imgDotElement from 'assets/images/bg/Landing_page_dot-element.png';
 // import imgEditIcon from 'assets/icons/edit-icon.png';
 // import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -26,7 +27,9 @@ import axios from 'axios';
 import { useToasts } from 'components/Toast/ToastProvider';
 import Loading from 'components/Loading';
 import searchIcon from 'assets/icons/search-icon.png';
-
+import SelectField from 'components/inputs/SelectField';
+import { ImgClose } from 'components/Sections/BasicDetails/PanUpload/ui';
+// import InputField from 'components/inputs/InputField';
 const Wrapper = styled.div`
 	padding: 30px 80px 50px 80px;
 	@media (max-width: 700px) {
@@ -335,6 +338,9 @@ export default function Products() {
 	const [errOTP, setErrOTP] = useState('');
 	const [addProduct, setAddProduct] = useState(false);
 	const [loadingOTP, setLoadingOTP] = useState(false);
+	const [isSubProductModalOpen,setSubProductModalOpen]= useState(false);
+	const [SubProduct, setSubProduct]= useState(false);
+	const [subProductComments,setSubProductCommnets]= useState(false);
 	const initialLoanProductCount = 3;
 	const permission = JSON.parse(sessionStorage.getItem('permission')) || {};
 
@@ -472,7 +478,19 @@ export default function Products() {
 			});
 		}
 	};
-
+	const options=[{
+		value:1,
+		name:'Product 1 and Product 2',
+	},
+	{
+		value:2,
+		name:'Product 2 and Product 2',
+	},
+	{
+		value:3,
+		name:'Product 3'
+	}
+]
 	useEffect(() => {
 		sessionStorage.removeItem('formstate');
 		sessionStorage.removeItem('formstatepan');
@@ -509,7 +527,9 @@ export default function Products() {
 			<ProductsBox style={{ marginTop: 40 }}>
 				{!addedProduct && products?.data?.length > 3 ? (
 					<AddProductBox>
-						<Button onClick={() => setAddProduct(true)} roundCorner>
+						<Button onClick={() =>{
+							// setAddProduct(true);
+							setSubProductModalOpen(true)}} roundCorner>
 							<FontAwesomeIcon size='sm' icon={faPlus} />
 							&nbsp;&nbsp;
 							<span className='text-blue'>Add Product</span>
@@ -556,6 +576,61 @@ export default function Products() {
 						</DivAdd>
 					</>
 				)}
+			</Modal>
+			<Modal
+			show= {isSubProductModalOpen}
+			onClose={()=>setSubProductModalOpen(false)}
+			width="40%"
+			>
+				<section  className='p-4 flex flex-col gap-y-8'>
+					<div style={{
+						display:'flex'
+					}}>
+						<span style={{
+							font:'30px Arial, sans-serif'
+						}}>Change Sub Product</span>
+					<ImgClose
+					onClick={()=>{
+						setSubProductModalOpen(false);
+					}}
+					src={imgClose}
+					alt='close'
+					/>
+					</div>
+					<div style={{
+						// display:'flex',
+						// gap:'10px',
+						// alignItems:'center',
+						fontSize:'large',
+						paddingTop:'1rem'
+					}}>
+						<h1>Select Sub-Product</h1>
+						<SelectField
+						options={options}
+						/>
+					</div>
+					<h1>Enter Comments*</h1>
+					<textarea
+					 className="comment-field"
+					 placeholder="Add Comments..."
+					 onChange={e=>setSubProductCommnets(e)}
+					 name="comment"
+					 id="comment"
+					 style={{
+						height:'150px',
+						border:'1px solid black'
+					 }}
+					/>
+					<Button
+					name='Submit'
+					 fill
+					 customStyle={{
+						display:'flex',
+						alignSelf:'flex-end',
+						justifyContent:'flex-end'
+					 }}
+					/>
+				</section>
 			</Modal>
 			<Modal show={modalOTP} width='50%' onClose={() => setModalOTP(false)}>
 				<ModalOTPHeader className='text-center w-full py-6'>
