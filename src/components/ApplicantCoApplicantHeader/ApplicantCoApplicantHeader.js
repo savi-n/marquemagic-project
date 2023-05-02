4bbab9052c1534a11d0eaada22113786d93eacb8
Akshat Sharma~ <akshat.sharma@namastecredit.com~>
2023-05-02 16:51:19 +0530

    Formatting

--- src/components/ApplicantCoApplicantHeader/ApplicantCoApplicantHeader.js
+++ src/components/ApplicantCoApplicantHeader/ApplicantCoApplicantHeader.js
@@ -167,9 +167,7 @@ const ApplicantCoApplicantHeader = props => {
 							onYes={() => {
 								setIsDeleteDirectorModalOpen(false);
 								dispatch(setAddNewDirectorKey(''));
-								dispatch(
-									setSelectedDirectorId(+Object.keys(directors)?.pop())
-								);
+								dispatch(setSelectedDirectorId(+Object.keys(directors)?.pop()));
 							}}
 							label={isDeleteDirectorModalOpen}
 						/>
