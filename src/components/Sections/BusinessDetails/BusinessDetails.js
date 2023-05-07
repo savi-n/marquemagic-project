709b8b938461545fb931723cbf9ebf7a74eb14e8
Shreyas <shreyas.ra@namastecredit.com>
2023-05-07 13:44:16 +0530

    minor changes

--- src/components/Sections/BusinessDetails/BusinessDetails.js
+++ src/components/Sections/BusinessDetails/BusinessDetails.js
@@ -707,8 +707,7 @@ const BusinessDetails = props => {
 											customFieldProps.disabled = true;
 										if (
 											field?.name === CONST.UDYAM_NUMBER_FIELD_NAME &&
-											formState?.values?.[CONST.UDYAM_NUMBER_FIELD_NAME] !==
-												undefined &&
+											!!formState?.values?.[CONST.UDYAM_NUMBER_FIELD_NAME] &&
 											`${formState?.values?.[CONST.UDYAM_NUMBER_FIELD_NAME]}`
 												?.length === 0
 										) {
