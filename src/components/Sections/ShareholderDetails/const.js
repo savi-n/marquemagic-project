f98f964b8efa71ef25e1920c249a662de296d0e4
bikash.mishra <bikash.mishra@namastecredit.com>
2023-05-29 14:42:12 +0530

    DOS-3931 shareholder details page partially done

---
+++ src/components/Sections/ShareholderDetails/const.js
@@ -0,0 +1,7 @@
+export const initialFormState = {
+	shareholder_name: 'Bikash',
+	company_address: 'Bangalore 560064',
+	shareholder_percentage: '50',
+	pincode: '560064',
+	relation_shareholder: 'Subsidiary Company',
+};
