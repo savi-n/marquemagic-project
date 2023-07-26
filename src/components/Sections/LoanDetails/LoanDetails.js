1197c1dacdcc6b1bb1832d18835e6a645e5a88bd
bikash.mishra <bikash.mishra@namastecredit.com>
2023-07-26 11:41:27 +0530

    DOS-4338 fixes for IMD colleted field updation

--- src/components/Sections/LoanDetails/LoanDetails.js
+++ src/components/Sections/LoanDetails/LoanDetails.js
@@ -530,6 +530,11 @@ const LoanDetails = () => {
 										if (isViewLoan) {
 											customFieldProps.disabled = true;
 										}
+
+										if(!(sectionData?.loan_details?.loan_status_id === 1 ) && newField?.name === CONST.IMD_COLLECTED_FIELD_NAME){
+											customFieldProps.disabled = true;
+										}
+
 										return (
 											<UI_SECTIONS.FieldWrapGrid
 												key={`field-${fieldIndex}-${newField.name}`}
