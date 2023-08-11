f9c9c70a54e2f5327c53941002c0ede16394fd23
Akshat Sharma~ <akshat.sharma@namastecredit.com~>
2023-08-11 16:15:26 +0530

    DOS-4381

--- src/components/Sections/BankDetails/DynamicForm.js
+++ src/components/Sections/BankDetails/DynamicForm.js
@@ -144,25 +144,21 @@ const DynamicForm = props => {
 			setIsSubmitting(false);
 		}
 	};
-	// useEffect(() => {
-	// 	if (
-	// 		!!formState?.values?.ifsc_code &&
-	// 		`${
-	// 			ifscList.filter(value => value.name === formState?.values?.ifsc_code)
-	// 				.length
-	// 		}` === '0'
-	// 	) {
-	// 		onChangeFormStateField({
-	// 			name: 'ifsc_code',
-	// 			value: '',
-	// 		});
-	// 		addToast({
-	// 			message: 'Please enter new IFSC code',
-	// 			type: 'error',
-	// 		});
-	// 	}
-	// 	//eslint-disable-next-line
-	// }, [ifscList]);
+	useEffect(() => {
+		if (
+			!!formState?.values?.ifsc_code &&
+			`${
+				ifscList.filter(value => value.name === formState?.values?.ifsc_code)
+					.length
+			}` === '0'
+		) {
+			onChangeFormStateField({
+				name: 'ifsc_code',
+				value: '',
+			});
+		}
+		//eslint-disable-next-line
+	}, [ifscList]);
 	// 	fields,
 	// 	app,
 	// 	selectedSection,
@@ -181,13 +177,15 @@ const DynamicForm = props => {
 					if (isViewLoan || isViewLoanApp) {
 						customFieldProps.disabled = true;
 					}
-					// console.log('render-field-', {
-					// 	field,
-					// 	customFieldProps,
-					// 	isViewLoan,
-					// 	newField,
-					// 	formState,
-					// });
+					// if(field.name.includes("account_holder_name")|| field.name.includes("ifsc")){
+					// 	customFieldProps.rules={
+					// 		"required": true
+					// 	}
+					// 	console.log('render-field-', {
+					// 		field,
+					// 		customFieldProps
+					// 	});
+					// }
 					return (
 						<UI_SECTIONS.FieldWrapGrid key={`field-${fieldIndex}`}>
 							{register({
