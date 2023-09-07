970116bc591a6136cb25f29872e8e2d0eb2376ff
bikash.mishra <bikash.mishra@namastecredit.com>
2023-09-07 14:03:00 +0530

    DOS-4631 changes in prefilling

--- src/components/Sections/BusinessDetails/BusinessDetails.js
+++ src/components/Sections/BusinessDetails/BusinessDetails.js
@@ -581,6 +581,7 @@ const BusinessDetails = props => {
 		}
 	};
 
+	console.log(formState.values, 'foram................');
 	const prefilledValues = field => {
 		try {
 			// TEST MODE
@@ -600,6 +601,13 @@ const BusinessDetails = props => {
 				business_email: sectionData?.user_data?.email,
 				email: sectionData?.business_details?.business_email,
 				name: sectionData?.business_details?.first_name,
+				industry_type:
+					sectionData?.business_details?.businessindustry?.IndustryName ||
+					sectionData?.business_details?.businessindustry?.id ||
+					'',
+
+				sub_industry_type:
+					sectionData?.business_details?.businessindustry?.id || '',
 			};
 			if (preData?.[field?.db_key]) return preData?.[field?.db_key];
 
