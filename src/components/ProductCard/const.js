38f7f7976e0d99df8d77ef32e9edb70eaba2cc41
Shreyas <shreyas.ra@namastecredit.com>
2023-09-14 18:07:43 +0530

    DOS-4723: Integrated fetch/search api based on the selected search customer field

---
+++ src/components/ProductCard/const.js
@@ -0,0 +1,6 @@
+export const SEARCH_CUSTOMER_USING_FIELD_VALUES = {
+	ucic_number: 'UCIC Number',
+	id_number: 'PAN/ Government ID and Mobile Number',
+};
+
+export const SEARCH_CUSTOMER_USING_FIELD_DB_KEY = 'search_customer_using';
