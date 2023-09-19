9863b7ab0a787b2999994ca3e026e71eb8dd14fc
bikash.mishra <bikash.mishra@namastecredit.com>
2023-09-19 18:50:11 +0530

    temp commit

--- src/components/Sections/LoanDetails/LoanDetails.js
+++ src/components/Sections/LoanDetails/LoanDetails.js
@@ -85,6 +85,358 @@ const LoanDetails = () => {
 		clearErrorFormState,
 	} = useForm();
 
+	const tempSelected = {
+		name: 'Loan Details',
+		id: 'loan_details',
+		hide_section_usertype: ['Technical'],
+		sub_sections: [
+			{
+				id: 'loan_details',
+				name: 'Loan Details',
+				fields: [
+					{
+						name: 'loan_amount',
+						placeholder: 'Required Loan Amount e.g.,10,000',
+						db_key: 'loan_amount',
+						rules: {
+							required: true,
+						},
+						type: 'number',
+						visibility: true,
+						inrupees: true,
+						mask: {
+							number_only: true,
+							character_limit: 10,
+						},
+					},
+					{
+						name: 'tenure',
+						rules: {},
+						placeholder: 'Tenure',
+						db_key: 'applied_tenure',
+						type: 'dropdown',
+						mask: {},
+						visibility: true,
+						specific_options_for: 'loan_amount',
+						specific_options: [
+							{
+								min: 25000,
+								max: 200000,
+								options: [
+									{
+										value: '104',
+										name: '104',
+									},
+								],
+							},
+							{
+								min: 200000,
+								max: 500000,
+								options: [
+									{
+										value: '104',
+										name: '104',
+									},
+									{
+										value: '156',
+										name: '156',
+									},
+									{
+										value: '190',
+										name: '180',
+									},
+								],
+							},
+							{
+								min: 500000,
+								max: 2000000,
+								options: [
+									{
+										value: '104',
+										name: '104',
+									},
+									{
+										value: '156',
+										name: '156',
+									},
+									{
+										value: '190',
+										name: '190',
+									},
+									{
+										value: '250',
+										name: '250',
+									},
+									{
+										value: '301',
+										name: '301',
+									},
+									{
+										value: '365',
+										name: '365',
+									},
+								],
+							},
+						],
+						options: [
+							{
+								value: '104',
+								name: '104',
+							},
+							{
+								value: '156',
+								name: '156',
+							},
+						],
+						sub_fields: [
+							{
+								name: 'tenure_um',
+								placeholder: 'Tenure',
+								type: 'text',
+								db_key: 'tenure_um',
+								rules: {},
+								options: [
+									{
+										name: 'days',
+										value: 'days',
+									},
+								],
+								value: 'days',
+								disabled: true,
+								isbuttonfilled: false,
+							},
+						],
+					},
+					{
+						name: 'loan_type_id',
+						placeholder: 'Purpose of the loan',
+						db_key: 'loan_type_id',
+						rules: {
+							required: false,
+						},
+						type: 'select',
+						options: [
+							{
+								name: 'Working Capital',
+								value: 10,
+							},
+							{
+								name: 'Business Improvement',
+								value: 60,
+							},
+						],
+						visibility: false,
+					},
+					{
+						name: 'loan_usage_type_id',
+						placeholder: ' Programs',
+						db_key: 'loan_usage_type_id',
+						rules: {
+							required: false,
+						},
+						type: 'select',
+						options: [
+							{
+								name: 'Assessment based',
+								value: 40,
+							},
+						],
+						value: '40',
+						visibility: false,
+					},
+				],
+			},
+			{
+				id: 'source_details',
+				name: 'Help us with Source Details',
+				fields: [
+					{
+						name: 'loan_source',
+						db_key: 'loan_origin',
+						placeholder: 'Loan Source',
+						rules: {},
+						options: [
+							{
+								name: 'Branch',
+								value: 'Branch',
+							},
+							{
+								name: 'Connector',
+								value: 'Connector',
+							},
+						],
+						type: 'select',
+						value: 'Branch',
+						visibility: true,
+					},
+					{
+						name: 'connector_name',
+						db_key: 'businessname',
+						placeholder: 'Connector Name',
+						for_type_name: 'loan_source',
+						for_type: ['Connector'],
+						rules: {},
+						options: [{}],
+						type: 'search',
+						visibility: true,
+					},
+					{
+						name: 'connector_code',
+						db_key: 'connector_user_id',
+						placeholder: 'Connector Code',
+						for_type_name: 'loan_source',
+						for_type: ['Connector'],
+						rules: {},
+						type: 'text',
+						visibility: true,
+					},
+				],
+			},
+			{
+				id: 'imd_details',
+				name: 'Help us with IMD Details',
+				fields: [
+					{
+						name: 'imd_collected',
+						db_key: 'imd_collected',
+						placeholder: 'IMD Collected',
+						rules: {},
+						options: [
+							{
+								name: 'Yes',
+								value: 'Yes',
+							},
+							{
+								name: 'No',
+								value: 'No',
+							},
+						],
+						type: 'select',
+						value: 'No',
+						visibility: true,
+					},
+					{
+						name: 'imd_document_proof',
+						db_key: 'loan_document',
+						label: 'Upload IMD Document Proof',
+						for_type: ['Yes'],
+						for_type_name: 'imd_collected',
+						type: 'file',
+						value: 'imd_doc',
+						is_delete_not_allowed: true,
+						min: 1,
+						max: 1,
+						rules: {
+							required: true,
+							supported_formats: ['*'],
+						},
+						doc_type: {
+							'1': 325,
+							'2': 325,
+							'3': 325,
+							'4': 325,
+							'5': 325,
+							'6': 325,
+							'8': 325,
+							'9': 325,
+							'10': 325,
+							'11': 325,
+						},
+						visibility: true,
+					},
+					{
+						name: 'amount_paid',
+						db_key: 'amount_paid',
+						placeholder: 'Amount Paid',
+						for_type: ['Yes'],
+						for_type_name: 'imd_collected',
+						type: 'text',
+						rules: {
+							required: true,
+						},
+						visibility: true,
+						inrupees: true,
+					},
+					{
+						name: 'mode_of_payment',
+						db_key: 'payment_mode',
+						for_type: ['Yes'],
+						for_type_name: 'imd_collected',
+						placeholder: 'Mode of Payment',
+						rules: {
+							required: true,
+						},
+						options: [
+							{
+								name: 'Bank Transfer',
+								value: 'Bank Transfer',
+							},
+							{
+								name: 'Cheque',
+								value: 'Cheque',
+							},
+							{
+								name: 'Cash',
+								value: 'Cash',
+							},
+							{
+								name: 'UPI',
+								value: 'UPI',
+							},
+							{
+								name: 'DD',
+								value: 'DD',
+							},
+						],
+						type: 'select',
+						visibility: true,
+					},
+					{
+						name: 'transaction_reference',
+						db_key: 'transaction_reference',
+						placeholder: 'Transaction Reference',
+						for_type: ['Yes'],
+						for_type_name: 'imd_collected',
+						type: 'text',
+						rules: {
+							required: true,
+						},
+						visibility: true,
+					},
+					{
+						name: 'imd_paid_by',
+						db_key: 'imd_paid_by',
+						for_type: ['Yes'],
+						for_type_name: 'imd_collected',
+						placeholder: 'IMD Paid By',
+						rules: {
+							required: true,
+						},
+						options: [
+							{
+								name: 'Others',
+								value: 'Others',
+							},
+						],
+						type: 'select',
+						visibility: true,
+					},
+					{
+						name: 'account_holder_name',
+						db_key: 'account_holder_name',
+						for_type: ['Others'],
+						for_type_name: 'imd_paid_by',
+						placeholder: 'Account Holder Name',
+						rules: {
+							required: true,
+						},
+						type: 'text',
+						visibility: true,
+					},
+				],
+			},
+		],
+	};
 	const prevSelectedConnectorId = useRef(null);
 
 	const selectedConnectorId =
@@ -519,7 +871,7 @@ const LoanDetails = () => {
 				<Loading />
 			) : (
 				<>
-					{selectedSection?.sub_sections?.map((sub_section, sectionIndex) => {
+					{tempSelected?.sub_sections?.map((sub_section, sectionIndex) => {
 						return (
 							<Fragment key={`section-${sectionIndex}-${sub_section?.id}`}>
 								{sub_section?.name ? (
@@ -552,6 +904,42 @@ const LoanDetails = () => {
 
 										if (newField?.name === CONST.BRANCH_FIELD_NAME) {
 											newField.options = branchOptions;
+										}
+
+										// if (
+										// 	newField?.name === CONST.TENURE &&
+										// 	`${formState.values[CONST.LOAN_AMOUNT]}` === '200000'
+										// ) {
+										// 	newField.options = newField?.options.filter(
+										// 		option => `${option.value}` !== '156'
+
+										// console.log(`${formState?.values?.[CONST.LOAN_AMOUNT]}`);
+										// 	newField.options =
+										// 		newField?.specific_options?.[
+										// 			`${formState?.values?.[CONST.LOAN_AMOUNT]}`
+										// 		] || newField?.options;
+										// 	);
+										// }
+
+										if (
+											newField?.name === CONST.TENURE &&
+											`${formState?.values?.[CONST.LOAN_AMOUNT]}`?.length > 0
+										) {
+											newField?.specific_options?.forEach(item => {
+												if (
+													Number(
+														formState?.values?.[newField?.specific_options_for]
+													) >= item.min &&
+													Number(
+														formState?.values?.[newField?.specific_options_for]
+													) <= item.max
+												) {
+													newField.options = item.options;
+													return;
+												} else {
+													newField.options = newField.options;
+												}
+											});
 										}
 
 										if (
