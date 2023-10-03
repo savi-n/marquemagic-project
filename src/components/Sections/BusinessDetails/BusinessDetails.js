9f4bba6c967ddac9f274a6842d2dbefb22883ab0
rahul.manas <rahul.manas@namastecredit.com>
2023-10-03 21:54:45 +0530

    Passing Parent Product ID

--- src/components/Sections/BusinessDetails/BusinessDetails.js
+++ src/components/Sections/BusinessDetails/BusinessDetails.js
@@ -274,6 +274,7 @@ const BusinessDetails = props => {
 				loan_product_id:
 					selectedProduct?.product_id?.[formState?.values?.['business_type']],
 				loan_product_details_id: selectedProduct?.id || undefined,
+				parent_product_id: selectedProduct?.parent_id || undefined,
 				loan_id: loanId,
 				business_id: businessId,
 				isApplicant: true, //implemented based on savitha's changes - bad practice
