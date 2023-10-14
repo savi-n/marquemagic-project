3344a08301aa137f3fe170c568d11e6979758513
Shreyas <shreyas.ra@namastecredit.com>
2023-10-15 00:16:11 +0530

    DOS-4947: dispatch only if the data is present from the api

--- src/components/Sections/BasicDetails/BasicDetails.js
+++ src/components/Sections/BasicDetails/BasicDetails.js
@@ -728,7 +728,7 @@ const BasicDetails = props => {
 			if (cacheDocumentsTemp.length > 0) {
 				try {
 					const uploadCacheDocumentsTemp = [];
-					cacheDocumentsTemp.map(doc => {
+					cacheDocumentsTemp?.map(doc => {
 						if (!doc?.requestId) return null;
 						uploadCacheDocumentsTemp.push({
 							...doc,
@@ -916,7 +916,7 @@ const BasicDetails = props => {
 						}`,
 						type: 'error',
 					});
-					return false;
+					return;
 				}
 			}
 
@@ -1551,12 +1551,20 @@ const BasicDetails = props => {
 					isEditOrViewLoan &&
 					`${selectedProduct?.loan_request_type}` === '2'
 				) {
-					const tempCompletedSections = parseJSON(
-						fetchRes?.data?.data?.trackData?.[0]?.onboarding_track
-					);
-					dispatch(
-						setNewCompletedSections(tempCompletedSections?.loan_details)
-					);
+					const tempTrackData = fetchRes?.data?.data?.trackData?.[0] || {};
+
+					const tempCompletedSections =
+						Object.keys(tempTrackData)?.length > 0 &&
+						JSON.parse(tempTrackData?.onboarding_track);
+
+					// const tempCompletedSections = parseJSON(
+					// 	fetchRes?.data?.data?.trackData?.[0]?.onboarding_track
+					// );
+					if (tempCompletedSections?.loan_details) {
+						dispatch(
+							setNewCompletedSections(tempCompletedSections?.loan_details)
+						);
+					}
 					if (
 						!tempCompletedSections?.loan_details?.includes(
 							CONST_SECTIONS.APPLICATION_SUBMITTED_SECTION_ID
@@ -1564,11 +1572,13 @@ const BasicDetails = props => {
 					) {
 						dispatch(setIsDraftLoan(true));
 					}
-					dispatch(
-						setNewCompletedDirectorSections(
-							tempCompletedSections?.director_details
-						)
-					);
+					if (tempCompletedSections?.director_details) {
+						dispatch(
+							setNewCompletedDirectorSections(
+								tempCompletedSections?.director_details
+							)
+						);
+					}
 				}
 
 				const fetchedProfilePicData =
