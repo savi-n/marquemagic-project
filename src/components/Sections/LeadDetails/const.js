43fe4e377e5641695b9d48c9e9edd1a459aeeffb
Shreyas <shreyas.ra@namastecredit.com>
2023-11-03 15:54:15 +0530

    DOS-5037: Introducing Lead details section

---
+++ src/components/Sections/LeadDetails/const.js
@@ -0,0 +1,10 @@
+export const initialFormState = {
+	name: 'const jijo',
+	last_name: 'Zangetsu',
+	pan_number: 'ABCDE2222F',
+	contact: '9999999999',
+	daadhaar: '123456789098',
+};
+
+// add all the field names as constants and use in the leads component
+//example -  export const FIRST_NAME_FIELD_NAME = 'first_name';
