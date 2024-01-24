7976c5b7fde4d6b22cb0857786e56b016533d85d
Kshithija - Namaste Credit <kshithija.s@namastecredit.com>
2024-01-24 17:41:33 +0530

    fix: code refactored

--- src/components/Sections/LeadDetails/LeadDetails.js
+++ src/components/Sections/LeadDetails/LeadDetails.js
@@ -793,30 +793,6 @@ const LeadDetails = props => {
 					}
 					firstName = nameSplit.join(' ');
 				}
-				//Company Search API
-				const companyNameSearchRes = await axios.post(API.SEARCH_COMPANY_NAME, {
-					search: panExtractionMsg?.upstreamName.trim(),
-				});
-
-				const newCompanyList = companyNameSearchRes?.data?.data?.[0] || [];
-
-				//ROC Data API
-				const cinNumberResponse =
-					newCompanyList?.CORPORATE_IDENTIFICATION_NUMBER &&
-					(await axios.post(
-						API.ROC_DATA_FETCH,
-						{ cin_number: newCompanyList?.CORPORATE_IDENTIFICATION_NUMBER },
-						{
-							headers: {
-								Authorization: clientToken,
-							},
-						}
-					));
-				const companyData = cinNumberResponse?.data?.data;
-				const companyStartDate =
-					companyData?.company_master_data?.date_of_incorporation;
-				const [date, month, year] = companyStartDate?.split(/\/|-/);
-				const bussinessStartDate = `${year}-${month}-${date}`;
 
 				if (`${selectedProduct?.loan_request_type}` === '2') {
 					onChangeFormStateField({
@@ -828,6 +804,34 @@ const LeadDetails = props => {
 						value: lastName || '',
 					});
 				} else {
+					//Company Search API
+					const companyNameSearchRes = await axios.post(
+						API.SEARCH_COMPANY_NAME,
+						{
+							search: panExtractionMsg?.upstreamName.trim(),
+						}
+					);
+
+					const newCompanyList = companyNameSearchRes?.data?.data?.[0] || [];
+
+					//ROC Data API
+					const cinNumberResponse =
+						newCompanyList?.CORPORATE_IDENTIFICATION_NUMBER &&
+						(await axios.post(
+							API.ROC_DATA_FETCH,
+							{ cin_number: newCompanyList?.CORPORATE_IDENTIFICATION_NUMBER },
+							{
+								headers: {
+									Authorization: clientToken,
+								},
+							}
+						));
+					const companyData = cinNumberResponse?.data?.data;
+					const companyStartDate =
+						companyData?.company_master_data?.date_of_incorporation;
+					const [date, month, year] = companyStartDate?.split(/\/|-/);
+					const bussinessStartDate = `${year}-${month}-${date}`;
+
 					onChangeFormStateField({
 						name: CONST.BUSINESS_NAME_FIELD_NAME,
 						value: name || '',
